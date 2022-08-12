80e5ea9cd75e00dce9c4d656a69c892e4c98c0b4
Mwalugha <mwalughabura@gmail.com>
2022-08-12 10:21:54 +0300

    Modified the certificate

--- app/views.py
+++ app/views.py
@@ -19,8 +19,8 @@ def certificate(request):
     pdf.cell(200, 8, f"{'Item'.ljust(30)} {'Amount'.rjust(20)}", 0, 1)
     pdf.line(10, 30, 150, 30)
     pdf.line(10, 38, 150, 38)
-    for line in sales:
-        pdf.cell(200, 8, f"{line['item'].ljust(30)} {line['amount'].rjust(20)}", 0, 1)
+    # for line in sales:
+    #     pdf.cell(200, 8, f"{line['item'].ljust(30)} {line['amount'].rjust(20)}", 0, 1)
 
     pdf.output('cert.pdf', 'F')
     return FileResponse(open('cert.pdf', 'rb'), as_attachment=True, content_type='application/pdf')
